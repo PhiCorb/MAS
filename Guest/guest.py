4b9b081cce5c50d0becfe95568945fea631c48f7
Phil Corbett <chubble.10@icloud.com>
2017-02-19 19:38:25 +0000

    Launcher initial ver

--- Guest/guest.py
+++ Guest/guest.py
@@ -1,1 +1,28 @@
+import json
+from time import sleep
+import sys
+import subprocess
+
+
+def progress_bar(value, endvalue, length=20):
+    percent = float(value) / endvalue
+    progress = '#' * int(round(percent * length))
+    spaces = ' ' * (length - len(progress))
+
+    sys.stdout.write("\r\tWaiting before execution: [{}] {}%".format(progress + spaces, int(round(percent * 100))))
+    sys.stdout.flush()
+
+base_path = "D:\\Phil Corbett\\Documents\\GitHub\\MAS\\Guest\\"
+json_name = "config.json"
+
+with open(base_path + json_name) as json_file:
+    config = json.load(json_file)
+
+if config["exec"]:  # Sample can be executed with subprocess.call()
+    print("Preparing to launch sample \"{}\".".format(config["sample"]))
+    for i in range(0, 6):
+        progress_bar(i, 5)
+        sleep(1)
+    subprocess.call(base_path + config["filename"], shell=True)
+
 # TODO: Add guest execution code
