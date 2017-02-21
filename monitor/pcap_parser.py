a1af8e1fbc916c65b4b28e4b2db6816c2e2f3b3b
Phil Corbett <chubble.10@icloud.com>
2017-02-21 12:26:21 +0000

    Add DNS support

--- monitor/pcap_parser.py
+++ monitor/pcap_parser.py
@@ -8,6 +8,17 @@ for x in cap:
         if x["http"].host not in hosts:
             hosts.append(x["http"].host)
 
-print("The following hosts were contacted:")
+print("The following hosts were contacted (HTTP):")
 for i in hosts:
+    print("\t" + i)
+
+cap = pyshark.FileCapture('/home/phil/Desktop/test1.cap', display_filter="dns")
+dns = []
+
+for x in cap:
+    if x["ip"].src == "172.16.168.128":
+        if x["dns"].qry_name not in dns:
+            dns.append(x["dns"].qry_name)
+print("The following DNS resolutions were requested:")
+for i in dns:
     print("\t" + i)
