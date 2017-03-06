0192ca599eae939be86fdd7b559592899b13f073
Phil Corbett <chubble.10@icloud.com>
2017-03-06 02:24:45 +0000

    Handler adjustments

--- websocket/handler.py
+++ websocket/handler.py
@@ -1,6 +1,7 @@
 from sys import stdout
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import datetime
 import json
 
 # # Count from 1 to 10 with a sleep
@@ -23,21 +24,23 @@ while True:
         print("{} {} {}".format(analysed, processing, waiting))
         stdout.flush()
     elif command == "active":
-        active_cursor = collection.find({"status": "Finished"}, {"filename": 1, "machine": 1, "date_time": 1})
+        active_cursor = collection.find({"status": "Running"}, {"filename": 1, "machine": 1, "date_time": 1})
         active_list = []
         for post in active_cursor:
             active_list.append(post)
         for post in active_list:
             post.update({"_id": str(post["_id"])})
+            post.update({"date_time": post["date_time"].strftime("%Y-%m-%d %H:%M:%S")})
         print(json.dumps(active_list))
         stdout.flush()
     elif command == "waiting":
-        active_cursor = collection.find({"status": "Finished"}, {"filename": 1, "machine": 1, "date_time": 1})
+        active_cursor = collection.find({"status": "Waiting"}, {"filename": 1, "machine": 1, "date_time": 1})
         active_list = []
         for post in active_cursor:
             active_list.append(post)
         for post in active_list:
             post.update({"_id": str(post["_id"])})
+            post.update({"date_time": post["date_time"].strftime("%Y-%m-%d %H:%M:%S")})
         print(json.dumps(active_list))
         stdout.flush()
     elif command == "jobs":
@@ -47,10 +50,13 @@ while True:
             active_list.append(post)
         for post in active_list:
             post.update({"_id": str(post["_id"])})
+            post.update({"date_time": post["date_time"].strftime("%Y-%m-%d %H:%M:%S")})
         print(json.dumps(active_list))
         stdout.flush()
     elif command == "details":
         target_id = input()
         active_post = collection.find_one({"_id": ObjectId(target_id)})
+        active_post.update({"_id": str(active_post["_id"])})
+        active_post.update({"date_time": active_post["date_time"].strftime("%Y-%m-%d %H:%M:%S")})
         print(json.dumps(active_post))
         stdout.flush()
