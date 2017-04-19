d57790311d6fc31ed2d15a476d7e5fea3d2b3bba
Phil Corbett <chubble.10@icloud.com>
2017-04-19 20:11:51 +0100

    Improved error handling in background.py

--- control/background.py
+++ control/background.py
@@ -34,52 +34,57 @@ while True:
         print("Found no samples queued")
         sleep(15)
     else:
-        sample_id = active_post["sample_id"]
-        sample_filename = active_post["filename"]
-        with open(sample_root + sample_filename, "wb") as outfile:
-            outfile.write(db.mongo.fs_get(sample_id).read())
-        extension = os.path.splitext(sample_root + sample_filename)[1]
-        if extension == ".exe" or extension == ".msi" or extension == ".bat":
-            executable = True
-        else:
-            executable = False
-        with open(sample_root + "config.json", "w") as json_file:
-            (json.dump({"executable": executable, "filename": sample_filename}, json_file, indent=4))
+        try:
+            sample_id = active_post["sample_id"]
+            sample_filename = active_post["filename"]
+            with open(sample_root + sample_filename, "wb") as outfile:
+                outfile.write(db.mongo.fs_get(sample_id).read())
+            extension = os.path.splitext(sample_root + sample_filename)[1]
+            if extension == ".exe" or extension == ".msi" or extension == ".bat":
+                executable = True
+            else:
+                executable = False
+            with open(sample_root + "config.json", "w") as json_file:
+                (json.dump({"executable": executable, "filename": sample_filename}, json_file, indent=4))
 
-        misc.progress_bar.progress_bar(0, 1, "Calculating sample MD5")
-        md5 = misc.md5.file(sample_root + sample_filename)
-        misc.progress_bar.progress_bar(1, 1, "Calculating sample MD5")
-        print("\nSample MD5: {}".format(md5))
+            misc.progress_bar.progress_bar(0, 1, "Calculating sample MD5")
+            md5 = misc.md5.file(sample_root + sample_filename)
+            misc.progress_bar.progress_bar(1, 1, "Calculating sample MD5")
+            print("\nSample MD5: {}".format(md5))
 
-        print("Spawning VM...")
-        if active_post["machine"] == "XP-1":
-            vm_path = vm_root + "XP VM 1/XP VM 1.vmx"
-        else:
-            print("Invalid machine in Mongo document.\nUsing XP-1.")
-            vm_path = vm_root + "XP VM 1/XP VM 1.vmx"
-        vm_control.spawn_vm(vm_path)
-        packet_capture.start_capture("172.16.168.128", capture_root + "cap.pcap")
-        db.mongo.modify(active_post["_id"], {"$set": {"status": "Running", "md5": md5, "addresses": [], "pcap": None}})
+            print("Spawning VM...")
+            if active_post["machine"] == "XP-1":
+                vm_path = vm_root + "XP VM 1/XP VM 1.vmx"
+            else:
+                print("Invalid machine in Mongo document.\nUsing XP-1.")
+                vm_path = vm_root + "XP VM 1/XP VM 1.vmx"
+            vm_control.spawn_vm(vm_path)
+            packet_capture.start_capture("172.16.168.128", capture_root + "cap.pcap")
+            db.mongo.modify(active_post["_id"], {"$set": {"status": "Running", "md5": md5, "addresses": [], "pcap": None}})
 
-        print("\n")
-        for i in range(0, int(active_post["duration"]) + 1):
-            misc.progress_bar.progress_bar(i, int(active_post["duration"]), "Waiting before terminating")
-            sleep(1)
+            print("\n")
+            for i in range(0, int(active_post["duration"]) + 1):
+                misc.progress_bar.progress_bar(i, int(active_post["duration"]), "Waiting before terminating")
+                sleep(1)
 
-        packet_capture.end_capture()
-        vm_control.end_vm(vm_path)
-        sleep(3)
-        vm_control.restore_vm(vm_path, "Snapshot")
+            packet_capture.end_capture()
+            vm_control.end_vm(vm_path)
+            sleep(3)
+            vm_control.restore_vm(vm_path, "Snapshot")
 
-        dns = pcap_parser.cap_dns(capture_root + "cap.pcap")
-        if os.path.getsize(capture_root + "cap.pcap") < 10000000:
-            http = pcap_parser.cap_http(capture_root + "cap.pcap")
-            dns += http
-        # http = pcap_parser.cap_http(capture_root + "cap.pcap")
-        with open(capture_root + "cap.pcap", "rb") as file:
-            pcap_id = db.mongo.fs_put(file)
+            dns = pcap_parser.cap_dns(capture_root + "cap.pcap")
+            if os.path.getsize(capture_root + "cap.pcap") < 10000000:
+                http = pcap_parser.cap_http(capture_root + "cap.pcap")
+                dns += http
+            with open(capture_root + "cap.pcap", "rb") as file:
+                pcap_id = db.mongo.fs_put(file)
 
-        db.mongo.modify(active_post["_id"], {"$set": {"status": "Finished", "addresses": dns, "pcap": pcap_id}})
-        db.mongo.modify(active_post["_id"], {"$unset": {"sample_id": ""}})
-        db.mongo.fs_delete(sample_id)
-        statsd.increment('samples.analysed', 1)
+            db.mongo.modify(active_post["_id"], {"$set": {"status": "Finished", "addresses": dns, "pcap": pcap_id}})
+            db.mongo.modify(active_post["_id"], {"$unset": {"sample_id": ""}})
+            db.mongo.fs_delete(sample_id)
+            statsd.increment('samples.analysed', 1)
+        except Exception as e:
+            print(colorama.Fore.WHITE + colorama.Back.RED + "Exception occurred during analysis: {}\n"
+                                                            "Setting {} to FAILED.".format(str(e), active_post["_id"])
+                                                            + colorama.Style.RESET_ALL)
+            db.mongo.modify(active_post["_id"], {"$set": {"status": "Failed"}})
